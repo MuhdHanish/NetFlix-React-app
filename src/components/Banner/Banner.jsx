a3e6c65e6b5109f67f7fc9ed8a830b23d6f47f39
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-12 12:06:53 +0530

    use state and use effect setted when the component  mount at the time the use effect work and it call the api usig axios and retrun the trending movies data , in the data it include the all things so we take title and desctription and image changed the css property and it set in the jsx file using style property and we set a base image url link in constant

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "../../axios";
+import {API_KEY,imgUrl} from "../../constance/constance";
 import "./Banner.css";
 
 function Banner() {
+  const [movie, setMovie] = useState({})
   useEffect(() => {
-    
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data.results[0])
+      setMovie(response.data.results[0])
+    })
     return () => {
       
     }
   }, [])
-  
   return (
-    <div className="banner">
+    <div style={{backgroundImage:`url(${movie?imgUrl+movie.backdrop_path:""})`}} className="banner">
       <div className="content">
-        <h1 className="title">Movie name</h1>
+        <h1 className="title">{movie? movie.title :""}</h1>
         <div className="banner-btn">
           <button className="btn">Play</button>
           <button className="btn">My List</button>
         </div>
         <h1 className="description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.voluptates error dolor quod, laborum quibusdam beatae dicta
+        {movie? movie.overview :""}
         </h1>
       </div>
       <div className="fade_bottom"></div>
