16e1e908ee240bde5d2521399a5b1cbcd02e9360
MuhdHanish <muhammedhanish11@gmail.com>
2023-05-12 10:55:48 +0530

    Axios intalled , api key and baseurl seted, axios instance configured

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Banner.css";
 
 function Banner() {
+  useEffect(() => {
+    
+    return () => {
+      
+    }
+  }, [])
+  
   return (
     <div className="banner">
       <div className="content">
@@ -14,9 +21,7 @@ function Banner() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit.voluptates error dolor quod, laborum quibusdam beatae dicta
         </h1>
       </div>
-      <div className="fade_bottom">
-      
-      </div>
+      <div className="fade_bottom"></div>
     </div>
   );
 }
---
+++ src/constance/constance.js
@@ -0,0 +1,2 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '18aafa5d7d953c929e5b78f2c42984d6';
